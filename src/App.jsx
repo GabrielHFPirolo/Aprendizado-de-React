de1299bfb51eac8c459803a71a549d49af0bb3c3
Gabriel Pirolo <gabrielpirolo25@gmail.com>
2026-04-04 23:01:30 -0300

    Uso de props e lógica componentes filhos com funções

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask.jsx";
 import Tasks from "./components/Tasks.jsx";
 import "./index.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "My first task",
+      description: "Learn React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "My Second task",
+      description: "Learn ....",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "My third task",
+      description: "Study ....",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((tasks) => {
+      if (tasks.id === taskId) {
+        return { ...tasks, isCompleted: !tasks.isCompleted };
+      }
+
+      return tasks;
+    });
+
+    setTasks(newTasks);
+  }
   //  State -> variável alterada que atualiza interface -> resposta interação user
   return (
-    <div>
-      <h1 className="text-red-500">Tasks Manager</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Tasks Manager
+        </h1>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <AddTask />
+      </div>
     </div>
   );
 }
